abfd34393c0490d25d1738a19f726b90b02d302d
Tom <hi@tomn.me>
2018-02-08 00:42:16 +1100

    It looks alright I think

--- static/app.js
+++ static/app.js
@@ -9,10 +9,75 @@
 
     // Generate some DOM elements
     var noShortUrlsRow = document.createElement('tr');
-    noShortUrlsRow.innerHTML = '<td colspan="3">No URLS found. Add some!</td>'
+    noShortUrlsRow.innerHTML = '<td colspan="3">No URLS found. Add some today!</td>';
+    noShortUrlsRow.style.display = 'none';
+    dataTable.appendChild(noShortUrlsRow);
 
     // Populate the table on page load
-    
+    // Quick row generating function which appends to table
+    function generateTableRow(record) {
+        var row = document.createElement('tr');
+        var short = document.createElement('td');
+        var long = document.createElement('td');
+        var actions = document.createElement('td');
+        var shortA = document.createElement('a');
+        shortA.text = record.short;
+        shortA.href = '/' + record.short;
+        short.appendChild(shortA);
+        long.appendChild(document.createTextNode(record.long));
+        
+        // Work on action buttons
+        var copyButton = document.createElement('button'); 
+        copyButton.className = 'pure-button button-success'; 
+        copyButton.innerHTML = 'c'; 
+        var deleteButton = document.createElement('button'); 
+        deleteButton.className = 'pure-button button-warning'; 
+        deleteButton.innerHTML = 'd';
+        deleteButton.addEventListener('click', function() {
+            deleteRecord(row);
+        });
+        actions.appendChild(copyButton);
+        actions.appendChild(deleteButton);
+
+        row.appendChild(short);
+        row.appendChild(long);
+        row.appendChild(actions);
+        dataTable.appendChild(row);
+    }
+
+    function deleteRecord(ref) {
+        var short = ref.children[0].children[0].text;
+        fetch('/api/delete/' + short, {
+            credentials: 'include',
+            method: 'DELETE'
+        }).then(function() {
+            dataTable.removeChild(ref);
+            if (dataTable.children.length <= 1) {
+                noShortUrlsRow.style.display = '';
+            }
+        }).catch(function(e) {
+            alert("An unknown error occurred, check the console for more details");
+            console.error(e);
+        });
+    }
+
+    // Request using fetch
+    fetch('/api/list', {credentials: 'include'}).then(function(data){return data.json()})
+        .then(function(res) {
+            // Use data to populate table
+            dataTable.removeChild(dataTable.children[0]);
+            // Needs to be synchronous
+            for (var i = 0; i < res.length; i++) {
+                generateTableRow(res[i]);
+            }
+            if (dataTable.children.length <= 1) {
+                noShortUrlsRow.style.display = '';
+            }
+        })
+        .catch(function(err) {
+            alert("An unknown error occurred, check the console for more details");
+            console.error(err);
+        });
 
     // Attach an event listener to the form 
     // (which submits the form and appends it to table)
@@ -31,8 +96,18 @@
             },
             body: JSON.stringify(data) 
         }).then(function(res){return res.json()})
-        .then(function(data){
-            console.log(data);
+        .then(function(res){
+            // Generate table row
+            if (res.status) {
+                generateTableRow({
+                    short: res.short,
+                    long: data.long
+                });
+                dataTable.children[0].style.display = 'none';
+            }
+        }).catch(function(err) {
+            alert("An unknown error occurred, check the console for more details");
+            console.error(err);
         });
     });
 })();
