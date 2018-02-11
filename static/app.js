20ca58712f4107de18a81c0551ade251c6633782
Tom <git@tomn.me>
2018-02-12 00:44:46 +1100

    Fix hacky client-side code

--- static/app.js
+++ static/app.js
@@ -1,3 +1,7 @@
+// Client side code
+// This is quite an abomination of manual DOM manipulation
+
+
 // Function wrapper
 (function(){
     // Initialise by grabbing the DOM elements
@@ -6,12 +10,18 @@
     var fInputLong = form.querySelector('[name=long]');
     var fInputOverwrite = form.querySelector('[name=overwrite]');
     var dataTable = document.getElementById('current-urls-data');
+    var dialog = document.getElementById('dialog');
 
     // Generate some DOM elements
     var noShortUrlsRow = document.createElement('tr');
     noShortUrlsRow.innerHTML = '<td colspan="3">No URLS found. Add some today!</td>';
     noShortUrlsRow.style.display = 'none';
     dataTable.appendChild(noShortUrlsRow);
+
+    // Hides the dialog (currently on timer)
+    function closeDialog() {
+       dialog.style.display = 'none'; 
+    }
 
     // Populate the table on page load
     // Quick row generating function which appends to table
@@ -63,8 +73,13 @@
             if (dataTable.children.length <= 1) {
                 noShortUrlsRow.style.display = '';
             }
+            dialog.className = 'dialog dialog-success';
+            dialog.innerHTML = 'Short URL <b>' + short + '</b> successfully created.';
+            dialog.style.display = '';
         }).catch(function(e) {
-            alert("An unknown error occurred, check the console for more details");
+            dialog.className = 'dialog dialog-error';
+            dialog.innerHTML = 'An unknown error occurred, check the console for more details.';
+            dialog.style.display = '';
             console.error(e);
         });
     }
@@ -99,7 +114,8 @@
         e.preventDefault();
         var data = {
             short: fInputShort.value,
-            long: fInputLong.value
+            long: fInputLong.value,
+            overwrite: fInputOverwrite.checked
         };
         fetch('/api/shorten', {
             credentials: 'include',
@@ -112,17 +128,59 @@
         .then(function(res){
             // Generate table row
             if (res.status) {
-                generateTableRow({
-                    short: res.short,
-                    long: data.long
-                });
+                if (data.overwrite) {
+                    // Check to see if anything can be overwritten.
+                    var found = false;
+                    for (var i of dataTable.children) {
+                        if (i.children.length == 1) {
+                            continue;
+                        }
+                        if (i.children[0].children[0].text == res.short) {
+                            found = true;
+                            i.children[1].children[0].href = data.long;
+                            i.children[1].children[0].text = data.long;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        generateTableRow({
+                            short: res.short,
+                            long: data.long
+                        });
+                    }
+                } else {
+                    generateTableRow({
+                        short: res.short,
+                        long: data.long
+                    });
+                }
+                dialog.className = 'dialog dialog-success';
+                dialog.innerHTML = 'Short URL <b>' + res.short + '</b> successfully created.';
+                dialog.style.display = '';
                 dataTable.children[0].style.display = 'none';
                 // Clear inputs
                 fInputShort.value = '';
                 fInputLong.value = '';
+            } else {
+                switch(res.error) {
+                    case 'ExistsShortURL':   
+                        dialog.innerHTML = 'Short URL already exists. Use overwrite to reuse the current name.';
+                        break
+                    case 'InvalidShortURL':
+                        dialog.innerHTML = 'Short URL is invalid.'
+                        break;
+                    default:
+                        dialog.innerHTML = 'An unknown error occurred, check the console for more details';
+                        console.error(res.error);
+                }   
+                dialog.className = 'dialog dialog-error';
+                dialog.style.display = '';
             }
+                
         }).catch(function(err) {
-            alert("An unknown error occurred, check the console for more details");
+            dialog.className = 'dialog dialog-error';
+            dialog.innerHTML = 'An unknown error occurred, check the console for more details.';
+            dialog.style.display = '';
             console.error(err);
         });
     });
